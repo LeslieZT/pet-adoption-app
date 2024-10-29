f205869caccf548a27a8b1df0b562a4207eefc35
LeslieZT <lfzarate30@gmail.com>
2024-10-28 22:05:02 -0500

    chore: initial setup

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+const config = {
+  useTabs: false,
+  tabWidth: 2,
+  endOfLine: "lf",
+  printWidth: 120,
+  arrowParens: "always",
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  singleAttributePerLine: true,
+  htmlWhitespaceSensitivity: "css",
+};
+
+export default config;
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Button } from "flowbite-react";
+
+
+function App() {
+  return (
+    <>      
+      <p className="text-blue-400">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div>
+      <Button>Click me</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
